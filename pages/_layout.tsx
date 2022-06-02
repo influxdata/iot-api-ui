87dd2a7b9cb770ca4fb52b459cfdec8e3966d0ea
Jason Stirnaman <jstirnaman@influxdata.com>
2022-06-02 10:03:54 -0500

    fix: Layout - Use the style module. - Remove footer.

--- pages/_layout.tsx
+++ pages/_layout.tsx
@@ -1,24 +1,17 @@
 import Head from 'next/head'
+import styles from '../styles/Home.module.css'
 
 export default function Layout({ children }) {
   return (
-    <div className="container">
+    <div className={styles.container}>
       <Head>
         <title>IoT Starter</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>{children}</main>
+      <main className={styles.main}>{children}</main>
 
       <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
-        </a>
       </footer>
 
       <style jsx global>{`
